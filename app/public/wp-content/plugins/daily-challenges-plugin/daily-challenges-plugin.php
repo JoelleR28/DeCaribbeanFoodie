a47633a9c735e022d3fe48cccee1d7caa2efe669
dj_Chery <122883959+DeepthiValachery@users.noreply.github.com>
2025-03-28 13:08:09 -0300

    Update structure of challenges

--- app/public/wp-content/plugins/daily-challenges-plugin/daily-challenges-plugin.php
+++ app/public/wp-content/plugins/daily-challenges-plugin/daily-challenges-plugin.php
@@ -8,24 +8,29 @@ Author URI: N/A
 License: GPL2
 */
 
-// // Prevent direct access
-// if (!defined('ABSPATH')) {
-//     exit;
-// }
-
-
 // Shortcode to Display Challenges
 function dc_display_challenges() {
-    $args = array('post_type' => 'challenge', 'posts_per_page' => 5);
+    $args = array(
+        'post_type' => 'challenge',
+        'posts_per_page' => 5
+    );
     $query = new WP_Query($args);
 
     if ($query->have_posts()) {
-        $output = '<ul>';
+        $output = '<div class="challenges-grid">';
         while ($query->have_posts()) {
             $query->the_post();
-            $output .= '<li><a href="' . get_permalink() . '"><strong>' . get_the_title() . '</strong></a>: ' . get_the_excerpt() . '</li>';
+            $output .= '<div class="challenge-card">';
+            if (has_post_thumbnail()) {
+                $output .= '<div class="challenge-img">' . get_the_post_thumbnail(null, 'medium') . '</div>';
+            }
+            $output .= '<div class="challenge-content">';
+            $output .= '<h2><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
+            $output .= '<p>' . get_the_excerpt() . '</p>';
+            $output .= '<a href="' . get_permalink() . '" class="btn">View Challenge</a>';
+            $output .= '</div></div>';
         }
-        $output .= '</ul>';
+        $output .= '</div>';
         wp_reset_postdata();
         return $output;
     } else {
@@ -33,3 +38,4 @@ function dc_display_challenges() {
     }
 }
 add_shortcode('daily_challenges', 'dc_display_challenges');
+
