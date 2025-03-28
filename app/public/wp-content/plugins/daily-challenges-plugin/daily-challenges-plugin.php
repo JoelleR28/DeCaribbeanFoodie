b34e7008da36aa6696c6d3e83159e61d7776e60f
dj_Chery <122883959+DeepthiValachery@users.noreply.github.com>
2025-03-28 13:15:37 -0300

    documentation

--- app/public/wp-content/plugins/daily-challenges-plugin/daily-challenges-plugin.php
+++ app/public/wp-content/plugins/daily-challenges-plugin/daily-challenges-plugin.php
@@ -39,6 +39,7 @@ function dc_display_challenges() {
 }
 add_shortcode('daily_challenges', 'dc_display_challenges');
 
+// Scedule a daily event
 function dc_schedule_daily_challenge() {
     if (!wp_next_scheduled('dc_publish_challenge')) {
         wp_schedule_event(time(), 'daily', 'dc_publish_challenge');
@@ -46,6 +47,7 @@ function dc_schedule_daily_challenge() {
 }
 add_action('wp', 'dc_schedule_daily_challenge');
 
+// Upload the oldest draft challenge
 function dc_publish_daily_challenge() {
     $args = array(
         'post_type' => 'challenge',
@@ -69,6 +71,7 @@ function dc_publish_daily_challenge() {
 }
 add_action('dc_publish_challenge', 'dc_publish_daily_challenge');
 
+// Tracks a user's completion
 function dc_mark_challenge_completed() {
     if (isset($_POST['complete_challenge'])) {
         $user_id = get_current_user_id();
