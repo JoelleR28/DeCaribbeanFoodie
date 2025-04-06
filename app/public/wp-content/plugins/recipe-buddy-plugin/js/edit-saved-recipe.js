afe9d8bef6dc26b06df489f7179c3a725d2d97b2
JoelleR28 <joelleramchandar2002@gmail.com>
2025-04-06 02:30:26 -0400

    save

--- app/public/wp-content/plugins/recipe-buddy-plugin/js/edit-saved-recipe.js
+++ app/public/wp-content/plugins/recipe-buddy-plugin/js/edit-saved-recipe.js
@@ -80,3 +80,58 @@ jQuery(document).ready(function($) {
         });
     });
 });
+
+
+//This jquery function handles the customised recipe buddy search bar.
+jQuery(document).ready(function($) {
+    //Enables live searching
+    let typingTimer;
+    const doneTypingInterval = 200; // Wait 300ms after user stops typing
+
+    $('#recipe-search-input').on('keyup', function() {
+        clearTimeout(typingTimer);
+        const searchTerm = $(this).val();
+
+        // Don't trigger search on empty input
+        if (searchTerm.length > 1) {
+            typingTimer = setTimeout(function() {
+                runLiveRecipeSearch(searchTerm);
+            }, doneTypingInterval);
+        } else {
+            $('#recipe-results').empty();
+        }
+    });
+
+    $('#recipe-search-form').submit(function(e) {
+        e.preventDefault(); // Prevent the default form submission
+        const searchTerm = $('#recipe-search-input').val();
+
+        // Check if searchTerm is not empty
+        if (searchTerm.length > 1) {
+            runLiveRecipeSearch(searchTerm);
+        } else {
+            $('#recipe-results').empty(); // Clear results when input is empty
+        }
+    });
+
+    //Retrieving data through a GET request 
+    function runLiveRecipeSearch(searchTerm) {
+        $.ajax({
+            url: recipeBuddyAjax.ajaxurl, // URL for AJAX handler
+            type: 'GET',
+            data: {
+                action: 'recipe_search', // The custom action for the search
+                s: searchTerm, // The search term, matching with html format
+                recipe_search: 1, // Ensures this is a Recipe Buddy search, not the default wp search bar
+            },
+            success: function(response) {
+                // Update the results div on the page with the matching search results
+                $('#recipe-results').html(response);
+            },
+            error: function() {
+                // Display error handling message if the request fails
+                $('#recipe-results').html('<p>Sorry, there was an error fetching the results. Please try again later.</p>');
+            }
+        });
+    }
+})
